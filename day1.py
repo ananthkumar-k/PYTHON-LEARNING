8739ac5c8bcfd731665bd6f413590e6bd271decf
Ananthkumar. k <159785401+ananthkumar-k@users.noreply.github.com>
2025-08-18 09:17:22 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,148 @@
+#Task 1
+#  num=int(input("Enter a number:"))
+# print(num)
+
+
+# Task 2
+# num=int(input("Enter a number:"))
+# if num>10:
+#     print("Bigger")
+# else:
+#     print("Smaller")
+
+#Task 3
+# num=int(input("Enter a number:"))
+# if num%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+#Task 4
+
+# num=int(input("Enter a number:"))
+# if num>0:
+#     print("Positive number")
+# elif num<0:
+#     print("Negative number")
+# else:
+#     print("Given value is 0")
+
+# Task 5
+
+# num=int(input("Enter a number:"))
+# if num>0:
+#     if num<100:
+#         print("The given value lesser than 100")
+#     else:
+#         print("The given value not lesser than 100")
+# else:
+#     print("Value is invalid")
+
+# Task 6
+
+# age=int(input("Enter your age:"))
+# height=int(input("Enter your height:"))
+# if age>=18 and height>=150:
+#     print("You can ride the roller coaster")
+# else:
+#     print("sorry, You can't ride")
+
+#Task 7
+
+# a=int(input("Enter tree1:"))
+# b=int(input("Enter tree2:"))
+# c=int(input("Enter tree3:"))
+# if a>b:
+#     if a>c:
+#         print("Tree1 is bigger")
+#     else:
+#         print("Tree3 is bigger")
+# elif b>a:
+#     if b>c:
+#         print("Tree2 is bigger")
+#     else:
+#         print("Tree3 is bigger")
+# else:
+#     print("All of them are equal")
+
+
+#TAsk 8
+# mark=int(input("Enetr your mark:"))
+# if mark>=50:
+#     if mark>=75:
+#         print("You got Distinction")
+#     else:
+#         print("You got pass mark")
+# else:
+#     print("YOU're FAil")
+
+#Task 9
+# num=int(input("Enter a number:"))
+# if num%3==0 and num%5==0:
+#     print("The given number is divisible by both 3 and 5")
+# else:
+#     print("The given number is not divisible by both 3 and 5 ")
+
+#Task 10
+# num=int(input("Enter a number:"))
+# if num>0:
+#     if num%2==1:
+#         print("The given number is odd and a Positive number")
+#     else:
+#         print("Doesn't match")
+# else:
+#     print("Given value is negative number")
+
+#Task 11
+
+# user_name=input("Enter your Username:")
+# password=input("Enter your Password:")
+# if user_name=="admin":
+#     if password=="1234":
+#         print("Login successfull")
+#     else:
+#         print("Wrong password")
+# else:
+#     print("Unknown username")
+
+#Task 12
+# num1=int(input("Enetr value 1:"))
+# num2=int(input("Enetr value 2:"))
+# if num1>num2:
+#     print("A is bigger")
+# elif num1==num2:
+#     print("Equal")
+# else:
+#     print("B is bigger")
+
+#Task 13
+
+# mon=input("Enter month name:")
+# if mon in["january","march","may","july","august","october","december"]:
+#     print("This month has 31 Days")
+# elif mon in["april","june","september","november"]:
+#     print("This month has 30 Days")
+# elif mon=="february":
+#     print("This month has 28 or 29 Days")
+# else:
+#     print("Invalid input")
+
+#Task 14
+# amount=int(input("Enter your amount"))
+# if amount>=1000:
+#     if amount>=5000:
+#         print("20% Discount")
+#     else:
+#         print("10% Discount")
+# else:
+#     print("You have no Discount this time")
+
+#Task 15
+# num=int(input("Enter a number:"))
+# if num%2==0:
+#     if num%4==0:
+#         print("Even and it is divisible by 4")
+#     else:
+#         print("Even but not divisible by 4")
+# else:
+#     print("Its an odd number")
